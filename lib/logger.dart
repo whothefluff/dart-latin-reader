81840b463e6340f545168dbf66d523c425e9ae33
Francisco Alcaraz <whothefluff@gmail.com>
2024-06-30 18:51:35 +0200

    clean logger

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,17 +1,24 @@
+// ignore_for_file: avoid_print
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 final Logger log = Logger('MyApp');
 
 void configureLogging() {
-  // Logger.root.level = Level.OFF;
-  Logger.root.onRecord.listen((record) {
-    print(
-        '${record.level.name}: ${record.time}: ${record.loggerName}: ${record.message}');
-    if (record.error != null) {
-      print('Error: ${record.error}');
-    }
-    if (record.stackTrace != null) {
-      print('StackTrace: ${record.stackTrace}');
-    }
-  });
+  if (kDebugMode) {
+    Logger.root.onRecord.listen(
+      (record) {
+        print(
+            '${record.level.name}: ${record.time}: ${record.loggerName}: ${record.message}');
+        if (record.error != null) {
+          print('Error: ${record.error}');
+        }
+        if (record.stackTrace != null) {
+          print('StackTrace: ${record.stackTrace}');
+        }
+      },
+    );
+  } else {
+    Logger.root.level = Level.OFF;
+  }
 }
