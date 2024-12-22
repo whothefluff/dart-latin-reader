958fe73d645486e24ca0a961deeb256911aa717f
Francisco Alcaraz <whothefluff@gmail.com>
2024-12-22 14:41:50 +0100

    improve logging

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,24 +1,37 @@
-// ignore_for_file: avoid_print
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
+import 'package:latin_reader/app_config.dart';
 import 'package:logging/logging.dart';
+import 'package:path_provider/path_provider.dart';
 
 final Logger log = Logger('MyApp');
 
 void configureLogging() {
-  if (kDebugMode) {
-    Logger.root.onRecord.listen(
-      (record) {
-        print(
-            '${record.level.name}: ${record.time}: ${record.loggerName}: ${record.message}');
-        if (record.error != null) {
-          print('Error: ${record.error}');
-        }
-        if (record.stackTrace != null) {
-          print('StackTrace: ${record.stackTrace}');
-        }
-      },
-    );
-  } else {
-    Logger.root.level = Level.OFF;
-  }
+  var writeSuccessful = true;
+  Logger.root.onRecord.listen((r) async {
+    final identifier = '${r.time} | ${r.level.name}';
+    final content = '${r.loggerName}: ${r.message}';
+    final message = '$identifier -> $content';
+    if (kDebugMode) {
+      print(message);
+      if (r.error != null) print('Error: ${r.error}');
+      if (r.stackTrace != null) print('StackTrace: ${r.stackTrace}');
+    }
+    if (writeSuccessful) {
+      try {
+        final d = await getApplicationDocumentsDirectory();
+        final file = File('${d.path}${Platform.pathSeparator}latin_reader.log');
+        final optError = r.error != null ? '\n${r.error}' : '';
+        final optStackTrace = r.stackTrace != null ? '\n${r.stackTrace}' : '';
+        await file.writeAsString('$message$optError$optStackTrace\n');
+      } on Exception catch (e) {
+        if (kDebugMode) print('Failed to write file log: $e');
+        writeSuccessful = false;
+      }
+    }
+  });
+  Logger.root.level = kDebugMode
+      ? AppConfig.instance.consoleLogLevel
+      : AppConfig.instance.fileLogLevel;
 }
